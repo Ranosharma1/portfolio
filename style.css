0938c99d58eda77fd111a8e97ea52f687a255f64
RANO SHARMA <23022099@geu.ac.in>
2026-06-24 11:15:45 +0530

    Create style.css

---
+++ style.css
@@ -0,0 +1,16 @@
+.cursor {
+    position: fixed;
+    width: 20 px;
+    height: 20 px;
+    border - radius: 50 % ;
+    background: #38bdf8;
+    pointer-events:none;
+    transform:translate(-50%,-50%);
+    filter:blur(8px);
+    opacity:.5;
+    z-index:9999;
+}
+
+.active{
+    color:# 38 bdf8!important;
+}
